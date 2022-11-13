489e80ba6c5683846ca24f7fb443b5f743cacefb
SoleGomez23 <solegomez1997@gmail.com>
2022-11-13 19:21:23 +0000

    creando proyecto

---
+++ cv-app/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+
+function App() {
+  return (
+    
+    <Routes>
+      <Route path="/" element={<Layout />} />
+    </Routes>
+      
+  );
+  
+}
+
+export default App;
---
+++ cv-app/src/components/Layout/index.js
@@ -0,0 +1,22 @@
+import Sidebar from '../Sidebar';
+import './index.scss';
+
+const Layout = () => {
+    return (
+        <div classname='App'>
+            <Sidebar />
+            <div className='page'>
+
+            <span className='tags top-tags'>
+
+            </span>
+
+            </div>
+        </div>
+        
+        
+    )
+   
+}
+
+export default Layout
